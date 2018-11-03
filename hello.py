af6aeabfd5e68964228ed28bd3f108179e29c623
Kirill <kirtrishin@gmail.com>
2018-11-03 17:14:19 +0300

    add sddg

--- hello.py
+++ hello.py
@@ -1,5 +1,4 @@
 def app(environ, start_response):
-    start_response('200 OK', [('Content-type', 'text/plain')])
-    return [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
-
-print()
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
+                  encoding="utf8")]
