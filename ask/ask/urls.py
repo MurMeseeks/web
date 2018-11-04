f802ef1462368fcd8472d74f4757c8cd80d9e5c1
Kirill <kirtrishin@gmail.com>
2018-11-04 14:14:51 +0300

    add

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^polls/', include('polls.urls')),
-    url(r'^qa/', include('qa.urls')),
+    url(r'^', include('qa.urls')),
+    url(r'^polls/', include('polls.urls')),    
     url(r'^admin/', admin.site.urls),
 ]
