6c6b6bf34efa15f29fc6491eb97fdb3287c87cdb
Kirill <kirtrishin@gmail.com>
2018-11-04 14:01:02 +0300

    add

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo -s ln -sf /home/box/web/etc/django-gunicorn.conf  /etc/gunicorn.d/django-gunicorn.conf
+sudo -s ln -sf /home/box/web/etc/gunicorn-django.conf  /etc/gunicorn.d/gunicorn-django.conf
 sudo gunicorn -w 4 -b 0.0.0.0:8080 hello:app &
 sudo gunicorn -w 4 -b 0.0.0.0:8000 /ask/ask.wsgi:application &
